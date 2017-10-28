23e8c3ca7bcbfd3eb6c2e5509fa82ac44377c9e1
Ariel Soares <arielsoares622@gmail.com>
2017-10-27 23:38:18 -0200

    Classes - Professor e Sala

---
+++ professor.php
@@ -0,0 +1,60 @@
+﻿<?php
+
+class Professor{
+    private $id;
+    private $nome;
+	private $email;
+	private $matricula;
+	private $bd;
+	
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+
+	public function getMatricula(){
+		return $this->matricula;
+	}
+
+	public function setMatricula($matricula){
+		$this->matricula = $matricula;
+	}
+	
+	public function getSenha(){
+		return $this->senha;
+	}
+
+	public function setSenha($senha){
+		$this->senha = $senha;
+	}
+	
+	// Método construtor da classe.
+	function __construct($id, $nome, $email, $matricula, $senha) {
+		$this->id = $id;
+		$this->nome = $nome;
+		$this->email = $email;
+		$this->matricula = $matricula;
+		$this->senha = $senha;
+	}	
+}   
+
+?>
---
+++ sala.php
@@ -0,0 +1,43 @@
+﻿<?php
+
+class Sala{
+    private $id;
+    private $nome;
+	private $codigo;
+	
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+
+
+	public function getCodigo(){
+		return $this->codigo;
+	}
+
+	public function setCodigo($codigo){
+		$this->codigo = $codigo;
+	}
+	
+	// Método construtor da classe.
+	function __construct($id, $nome, $codigo) {
+		$this->id = $id;
+		$this->nome = $nome;
+		$this->codigo = $codigo;
+	
+	} 
+}	
+	  
+
+?>
